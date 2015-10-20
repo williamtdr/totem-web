3ad3da06cd9bdbc0ee9cb39ba47f375183d3235c
William Teder <wteder@hydreon.com>
2015-10-20 15:45:00 -0500

    Revert "Update background.js"
    This reverts commit 399d0f53ccc42c8d5f0524b721167e94e0941905.

--- js/common/background.js
+++ js/common/background.js
@@ -10,12 +10,12 @@ $("#background_layer").append('<div class="top"></div><div class="bottom"></div>
 advanceBackgroundImage = function() {
     var top_img = $(".top");
     var bottom_img = $(".bottom");
-    
+
     if(last_image_placed == 2) {
         top_img.attr('style', 'background: url("http://d35y9swri9q567.cloudfront.net/background/' + getRandomInt(1, 572) + '.jpg") no-repeat center center fixed; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover;');
         last_image_placed = 1;
     } else {
-        bottom_img.attr('style', 'background: url("http://dcvslab.github.io/backgrounds/' + getRandomInt(1, 699) + '.jpeg") no-repeat center center fixed; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover;');
+        bottom_img.attr('style', 'background: url("http://d35y9swri9q567.cloudfront.net/background/' + getRandomInt(1, 572) + '.jpg") no-repeat center center fixed; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover;');
         last_image_placed = 2;
     }
 
