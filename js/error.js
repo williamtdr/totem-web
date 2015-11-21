74724e817f3bb2d7d5a3360464786927fbffcd1b
William Teder <wteder@hydreon.com>
2015-11-20 18:56:47 -0600

    Format & split error code

---
+++ js/error.js
@@ -0,0 +1,18 @@
+function getRandomInt(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+document.getElementById("background_layer").setAttribute('style', 'background: url("http://d35y9swri9q567.cloudfront.net/background/' + getRandomInt(1, 572) + '.jpg") no-repeat center center fixed; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover;');
+var time = 10;
+var timer = document.getElementById("time");
+function countDown() {
+	setTimeout(function () {
+		time = time - 1;
+		if (time == 0) {
+			location.href = "http://totem.fm";
+		}
+		timer.innerHTML = time;
+		countDown();
+	}, 1000);
+}
+countDown();
