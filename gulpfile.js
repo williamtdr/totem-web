f2c3df8b7103a2c41e2f025608727a29e6c4d9f7
William Teder <wteder@hydreon.com>
2015-12-03 18:37:54 -0600

    Remove manual gzipping because we're switching away from s3

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,12 @@
 var gulp = require('gulp'),
 	minifyCss = require('gulp-minify-css'),
 	concat = require('gulp-concat'),
-	gzip = require('gulp-gzip'),
 	uglify = require('gulp-uglify');
 
 gulp.task('minify-css', function() {
 	return gulp.src('css/**/*.css')
 		.pipe(minifyCss({compatibility: 'ie8'}))
 		.pipe(concat('totem.min.css'))
-		.pipe(gulp.dest('dist'))
-		.pipe(gzip())
 		.pipe(gulp.dest('dist'));
 });
 
@@ -17,8 +14,6 @@ gulp.task('minify-js', function() {
 	return gulp.src('js/**/*.js')
 		.pipe(uglify())
 		.pipe(concat('totem.min.js'))
-		.pipe(gulp.dest('dist'))
-		.pipe(gzip())
 		.pipe(gulp.dest('dist'));
 });
 
